3ae265592318adb5c8c188d84075fec211f5bba3
b.m.ulyashev <b.m.ulyashev@mail.ru>
2022-10-20 19:11:43 +0300

    Сделаны тесты для FacultyController.

--- src/test/java/ru/skypro/school/FacultyControllerTest.java
+++ src/test/java/ru/skypro/school/FacultyControllerTest.java
@@ -14,16 +14,23 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.skypro.school.component.RecordMapper;
 import ru.skypro.school.controller.FacultyController;
 import ru.skypro.school.entity.Faculty;
+import ru.skypro.school.entity.Student;
 import ru.skypro.school.record.FacultyRecord;
+import ru.skypro.school.record.StudentRecord;
 import ru.skypro.school.repository.FacultyRepository;
 import ru.skypro.school.service.FacultyService;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = FacultyController.class)
 @ExtendWith(MockitoExtension.class)
@@ -114,8 +121,88 @@ public class FacultyControllerTest {
                         .content(json.toString())
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound());
+                .andExpect(status().isNotFound())
+                .andExpect(result -> {
+                    assertThat(result.getResponse().getContentAsString(StandardCharsets.UTF_8))
+                            .isEqualTo("Факультет с id = 1 не найден!");
+                });
+    }
 
+    @Test
+    public void delete() throws Exception {
+        Faculty faculty = createFaculty(1, "test", "black");
+        FacultyRecord facultyRecord = createFacultyRecord(1, "test", "black");
+
+        when(facultyRepository.findById(any()))
+                .thenReturn(Optional.of(faculty))
+                .thenReturn(Optional.empty());
+        doNothing().when(facultyRepository).deleteById(any());
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/faculties/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(facultyRecord.getName()))
+                .andExpect(jsonPath("$.color").value(facultyRecord.getColor()))
+        ;
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/faculties/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(result -> {
+                    assertThat(result.getResponse().getContentAsString(StandardCharsets.UTF_8))
+                            .isEqualTo("Факультет с id = 1 не найден!");
+                });
+    }
+
+    @Test
+    public void findByFilterString() throws Exception {
+        List<Faculty> faculties = List.of(
+                createFaculty(1, "1", "red"),
+                createFaculty(3, "3", "red")
+        );
+
+        when(facultyRepository.findByNameLikeIgnoreCaseOrColorLikeIgnoreCase(any(), any()))
+                .thenReturn(faculties)
+                .thenReturn(Collections.emptyList());
+
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/faculties?filterString=red")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[*].color").value(containsInAnyOrder("red", "red")));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/faculties?filterString=")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("[]"));
+    }
+
+    @Test
+    public void findStudent() throws Exception {
+        List<Student> students = List.of(
+                createStudent(1, "1", 17),
+                createStudent(3, "3", 18)
+        );
+
+        Faculty faculty = createFaculty(1, "test", "red");
+        faculty.setStudents(students);
+
+        when(facultyRepository.findById(any())).thenReturn(Optional.of(faculty));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/faculties/{id}/students", 1)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[*].name").value(containsInAnyOrder("1", "3")));
     }
 
     private Faculty createFaculty(long id, String name, String color) {
@@ -134,5 +221,20 @@ public class FacultyControllerTest {
         return facultyRecord;
     }
 
+    private Student createStudent(long id, String name, int age) {
+        Student student = new Student();
+        student.setId(id);
+        student.setName(name);
+        student.setAge(age);
+        return student;
+    }
+
+    private StudentRecord createStudentRecord(long id, String name, int age) {
+        StudentRecord studentRecord = new StudentRecord();
+        studentRecord.setId(id);
+        studentRecord.setName(name);
+        studentRecord.setAge(age);
+        return studentRecord;
+    }
 
 }
