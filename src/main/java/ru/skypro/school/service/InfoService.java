6eab9cbfb862353f746ebc81c416d6eb551f1394
b.ulyashev <b.m.ulyashev@mail.ru>
2022-10-27 12:17:27 +0300

    Добавлен профиль.

---
+++ src/main/java/ru/skypro/school/service/InfoService.java
@@ -0,0 +1,20 @@
+package ru.skypro.school.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InfoService {
+
+    Logger logger = LoggerFactory.getLogger(InfoService.class);
+    @Value("${server.port}")
+    private int serverPort;
+
+    public int getPort() {
+        logger.info("Invoke get port value");
+        logger.debug("server.port = {}", serverPort);
+        return serverPort;
+    }
+}
